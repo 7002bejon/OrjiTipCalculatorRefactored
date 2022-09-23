eb39644cee579990cc6dbd1de3875e85d7c332f2
1E10 Laptop <sample@bths.edu>
2022-09-23 12:18:14 -0400

    Fixed

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -11,19 +11,27 @@ public class TipCalculator {
         int numberofPeople = scan.nextInt();
         System.out.println("What is the tip percentage? ");
         int tipPercentage = scan.nextInt();
+        String formattedPercentage = formatter.format(tipPercentage);
 
         double num = 0;
-        double billBeforetip = 0;
+        double billBeforetip = num;
         while ( num != -1) {
             System.out.println("Enter the item cost, Enter -1 to end: ");
             num = scan.nextDouble();
             billBeforetip = billBeforetip + num;
         }
-        double tipAmount = billBeforetip * (tipPercentage / 100);
+
+        
+        double tipAmount = billBeforetip * ((double) tipPercentage / 100);
+        String formattedtipAmount = formatter.format(tipAmount);
         double totalBill = tipAmount + billBeforetip;
+        String formattedtotalBill = formatter.format(totalBill);
         double perpersonBeforetip = billBeforetip / numberofPeople;
+        String formattedperpersonBeforetip = formatter.format(perpersonBeforetip);
         double perpersontipAmount = tipAmount  /  numberofPeople;
+        String formattedperpersontipAmount = formatter.format(perpersontipAmount);
         double perpersonTotal = totalBill / numberofPeople;
+        String formattedperpersonTotal = formatter.format(perpersonTotal);
 
         System.out.println("Total Bill before Tip: " + billBeforetip);
         System.out.println("Total Bill after Tip: " + totalBill);
@@ -32,8 +40,6 @@ public class TipCalculator {
         System.out.println("Per Person Cost Before Tip " + perpersonBeforetip);
         System.out.println("Per Person Tip Amount: " + perpersontipAmount);
         System.out.println("Per Person Total Cost " + perpersonTotal);
-
-
 
     }
 }
